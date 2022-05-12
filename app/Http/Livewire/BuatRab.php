64c07b268f34737294c4a4846bb513813b9fa637
IqbalSalim <iqbalbinsalim@gmail.com>
2022-05-12 23:19:31 +0800

    a

--- app/Http/Livewire/BuatRab.php
+++ app/Http/Livewire/BuatRab.php
@@ -73,7 +73,7 @@ class BuatRab extends Component
 
     public function store()
     {
-        dd(json_decode($this->posts['ruangan'][0])->ruangan);
+
         // Perbaikan Strukrut Array
         foreach ($this->posts as $key => $value) {
             $i = 0;
